0f69bf8d592d83ead35445529ebe06b1a26f4289
Poldas <poldas69@gmail.com>
2025-12-09 23:44:52 +0100

    budowanie obrazu

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -e
 
-# TODO: pamiętać o usunięciu, to jest tylko do testów lokalnych
-# Ensure an .env file exists so Symfony Dotenv does not fail when APP_ENV is set
-[ -f /var/www/html/.env ] || touch /var/www/html/.env
-
 # Warm cache using real runtime environment variables (prod only)
 if [ "${APP_ENV:-dev}" = "prod" ]; then
   php bin/console cache:clear --no-warmup --env=prod
